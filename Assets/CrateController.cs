599d008346e348a96e79435302ed3b2b9574dce2
aeric90 <51760291+aeric90@users.noreply.github.com>
2022-11-01 10:01:40 -0400

    Crate destruction and respawn

---
+++ Assets/CrateController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+enum CRATESTATE
+{
+    MOVING,
+    DEAD
+}
+
+public class CrateController : MonoBehaviour
+{
+    private GameObject crateSpawn;
+    private float deathTime;
+    private float spawnTime = 2.0f;
+
+    private List<int> checkPoints = new List<int>();
+
+    private CRATESTATE crateState = CRATESTATE.MOVING;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        crateSpawn = GameObject.Find("Cube Spawn 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (crateState)
+        {
+            case CRATESTATE.MOVING:
+                break;
+            case CRATESTATE.DEAD:
+                // Change to a dead animation
+                if (Time.time - deathTime >= spawnTime) Respawn();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "destroy")
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            deathTime = Time.time;
+            crateState = CRATESTATE.DEAD;
+        }
+        if (other.gameObject.tag == "checkpoint")
+        {
+            if (!checkPoints.Contains(other.gameObject.GetComponent<CheckpointController>().checkPointID))
+            {
+                checkPoints.Add(other.gameObject.GetComponent<CheckpointController>().checkPointID);
+                crateSpawn = other.gameObject.GetComponent<CheckpointController>().cubeSpawn;
+            }
+        }
+    }
+
+    private void Respawn()
+    {
+        this.transform.position = crateSpawn.transform.position;
+        this.transform.rotation = crateSpawn.transform.rotation;
+        crateState = CRATESTATE.MOVING;
+    }
+}
