e55ee07456c079b498bf2162ae8312a3fe97df52
aeric90 <ericdemarbre@cmail.carleton.ca>
2022-11-02 08:00:31 -0400

    Completed Beta Build
    Could not figure out the 3rd person camera view clamp in time. I unrestricted the camera for now. I'll implement that later.

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -6,13 +6,32 @@ public class CameraController : MonoBehaviour
 {
     public float CameraXMax = 0.0f;
     public float CameraXMin = 40.0f;
+    private float lastX;
 
     // Update is called once per frame
     void Update()
     {
+        /*
         float x = transform.localEulerAngles.x;
 
-        if (x < CameraXMax) Camera.main.transform.localEulerAngles = new Vector3(CameraXMax, Camera.main.transform.localEulerAngles.y, 0);
-        if (x > CameraXMin && x < 355.0f) Camera.main.transform.localEulerAngles = new Vector3(CameraXMin, Camera.main.transform.localEulerAngles.y, 0);
+        if(CameraXMax < 0.0f)
+        {
+            if (x > 360.0f + CameraXMax)
+            {
+                Debug.Log("SET MAX " + x);
+                Camera.main.transform.localEulerAngles = new Vector3(360.0f + CameraXMax, Camera.main.transform.localEulerAngles.y, 0);
+            }
+            else if (x > CameraXMin && x < 360.0f + CameraXMax)
+            {
+                Camera.main.transform.localEulerAngles = new Vector3(CameraXMin, Camera.main.transform.localEulerAngles.y, 0);
+            }
+        } 
+        else
+        {
+            if (x < CameraXMax) Camera.main.transform.localEulerAngles = new Vector3(CameraXMax, Camera.main.transform.localEulerAngles.y, 0);
+            else if (x > CameraXMin) Camera.main.transform.localEulerAngles = new Vector3(CameraXMin, Camera.main.transform.localEulerAngles.y, 0);
+        }
+
+     */   
     }
 }
